4fea282c6f3f8e27eeef29e666bfb5c31796b5d3
Antoine Loiseau <anloisea@c2r3p1.42nice.fr>
2023-03-16 12:30:39 +0100

    ex06 part2

---
+++ ex06/filterstring.py
@@ -0,0 +1,15 @@
+import sys
+from ft_filter import ft_filter
+
+if __name__ == "__main__":
+    sys.tracebacklimit = 0
+
+    assert len(sys.argv) == 3, "the arguments are bad"
+    assert type(sys.argv[1]) is str, "the arguments are bad"
+    assert sys.argv[2].isnumeric(), "the arguments are bad"
+
+    S = sys.argv[1]
+    N = (int)(sys.argv[2])
+    lst = S.split()
+    filtered_string = ft_filter(lambda string: len(string) >= N, lst)
+    print(list(filtered_string))
